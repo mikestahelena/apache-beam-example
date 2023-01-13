b1c8f9b96065447bbcd669961d0afdbb0106fa00
michael.helena <michael.helena@cwi.com.br>
2023-01-13 17:05:10 -0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
+
+import java.util.Arrays;
+
+public class Main {
+    public interface Options extends StreamingOptions {
+		@Description("Input text to print.")
+		@Default.String("My input text")
+		String getInputText();
+
+		void setInputText(String value);
+	}
+
+	public static PCollection<String> buildPipeline(Pipeline pipeline, String inputText) {
+        return pipeline
+                .apply("Create elements", Create.of(Arrays.asList("Hello", "World!", inputText)))
+                .apply("Print elements",
+                        MapElements.into(TypeDescriptors.strings()).via(x -> {
+                            System.out.println(x);
+                            return x;
+                        }));
+    }
+
+	public static void main(String[] args) {
+		var options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+		var pipeline = Pipeline.create(options);
+		Main.buildPipeline(pipeline, options.getInputText());
+		pipeline.run().waitUntilFinish();
+	}
+}
